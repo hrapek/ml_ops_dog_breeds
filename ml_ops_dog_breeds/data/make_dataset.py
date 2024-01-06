ffa238c7a506e5f0d3b8914c8fab19ef022b2b67
samyhaff <haffoudhisamy@gmail.com>
2024-01-06 22:32:24 +0100

    updated make_dataset script to save images

--- ml_ops_dog_breeds/data/make_dataset.py
+++ ml_ops_dog_breeds/data/make_dataset.py
@@ -1,10 +1,13 @@
 from typing import Dict
 import csv
 import torch
+from torch.utils.data import DataLoader
 from torchvision import transforms
 from dataset import DogBreedDataset
 
 # TODO logging
+# TODO check transformations
+# TODO handle labels
 
 def read_labels(filepath: str) -> Dict[str, str]:
     """Read the (image, labels) tuples from the csv file into a dictionary."""
@@ -22,10 +25,18 @@ if __name__ == '__main__':
     labels = read_labels('data/raw/labels.csv')
 
     transform = transforms.Compose([
-        # transforms.Resize(256),
+        transforms.Resize((224, 224)),
         # transforms.CenterCrop(224),
         transforms.ToTensor()
     ])
 
     dataset = DogBreedDataset(labels, 'data/raw/train/', transform=transform)
-    torch.save(dataset, 'data/processed/dataset.pt')
+
+    batch_size = 32
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    all_images = []
+    for images, labels in data_loader:
+        all_images.append(images)
+
+    torch.save(all_images, 'data/processed/images.pt')
