86383ef3fe56554810374c401f416e13bd9927f9
samyhaff <haffoudhisamy@gmail.com>
2024-01-08 21:29:15 +0100

    added basic training loop

--- ml_ops_dog_breeds/train_model.py
+++ ml_ops_dog_breeds/train_model.py
@@ -11,9 +11,23 @@ BATCH_SIZE = 32 # TODO config
 LEARNING_RATE = 0.001 # TODO config
 EPOCHS = 10 # TODO config
 
-if __name__ == '__main__':
-    model = create_model('resnet50', num_classes=NUM_CLASSES)
+def train(model, train_loader, epochs, loss_fn, optimizer, device):
+    model.train()
+    for epoch in range(epochs):
+        for images, labels in train_loader:
+            images = images.to(device)
+            labels = labels.to(device)
 
+            outputs = model(images)
+            loss = loss_fn(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            print(f'Epoch {epoch+1}/{epochs}, loss: {loss.item():.4f}')
+
+if __name__ == '__main__':
     images_train = torch.load('data/processed/images_train.pt')
     labels_train = torch.load('data/processed/labels_train.pt')
 
@@ -21,5 +35,8 @@ if __name__ == '__main__':
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = create_model('resnet50', num_classes=NUM_CLASSES).to(device)
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train(model, train_loader, EPOCHS, loss_fn, optimizer, device)
