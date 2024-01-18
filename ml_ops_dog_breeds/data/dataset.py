aede0ac5a4881d4ed04222b98cc294db54ce81d1
Małgorzata Korona <malgorzatakorona1@gmail.com>
2024-01-19 00:18:22 +0100

    Update data paths

--- ml_ops_dog_breeds/data/dataset.py
+++ ml_ops_dog_breeds/data/dataset.py
@@ -9,12 +9,10 @@ from sklearn.preprocessing import LabelEncoder
 
 import os
 
-#_DATASCRIPTS_ROOT = os.path.dirname(__file__)  # folder of this script == ml_ops_dog_breeds/ml_ops_dog_breeds/data
-#_PROJECT_ROOT = os.path.dirname(_DATASCRIPTS_ROOT)  # root of project == ml_ops_dog_breeds/ml_ops_dog_breeds
-#_FOLDER_ROOT = os.path.dirname(_PROJECT_ROOT)  # root of folder == ml_ops_dog_breeds
-#_PATH_DATA = os.path.join(_FOLDER_ROOT, 'data')  # path of data (not this data folder, but global)
-_PATH_ROOTFOLDER = os.path.join(os.path.dirname(__file__).split('ml_ops_dog_breeds')[0], 'ml_ops_dog_breeds')
-_PATH_DATA = os.path.join(_PATH_ROOTFOLDER, 'data')
+_DATASCRIPTS_ROOT = os.path.dirname(__file__)  # folder of this script == ml_ops_dog_breeds/ml_ops_dog_breeds/data
+_PROJECT_ROOT = os.path.dirname(_DATASCRIPTS_ROOT)  # root of project == ml_ops_dog_breeds/ml_ops_dog_breeds
+_FOLDER_ROOT = os.path.dirname(_PROJECT_ROOT)  # root of folder == ml_ops_dog_breeds
+_PATH_DATA = os.path.join(_FOLDER_ROOT, 'data')  # path of data (not this data folder, but global)
 
 class DogBreedsDataModule(LightningDataModule):
     """Lightning DataModule for loading and processing Dog Breeds classification data.
