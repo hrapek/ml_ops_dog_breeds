869b0e996eba8e7f8fe92b167428c96679ca62f2
Małgorzata Korona <malgorzatakorona1@gmail.com>
2024-01-18 17:33:23 +0100

    Access model from bucket in api and update cloudbuild for trigger workflows

--- ml_ops_dog_breeds/api/app/main.py
+++ ml_ops_dog_breeds/api/app/main.py
@@ -6,10 +6,23 @@ from ml_ops_dog_breeds.data.dataset import DogBreedsDataModule
 from io import BytesIO
 import joblib
 import torch
-
+from google.cloud import storage
 
 app = FastAPI()
 
+BUCKET_NAME = 'mlops-project-model-mk'
+MODEL_FILE = 'models/epoch=29-step=3840.ckpt'
+LABELS_FILE = 'data/processed/label_encoder.pkl'
+
+# client = storage.Client()
+# bucket = client.get_bucket(BUCKET_NAME)
+# blob_model = bucket.get_blob(MODEL_FILE)
+# model = MyNeuralNet.load_from_checkpoint(BytesIO(blob_model.download_as_bytes()))
+# blob_data = bucket.get_blob(LABELS_FILE)
+# label_encoder = joblib.load(BytesIO(blob_data.download_as_bytes()))
+
+model = MyNeuralNet.load_from_checkpoint(MODEL_FILE)
+label_encoder = joblib.load(LABELS_FILE)
 
 @app.get('/')
 def root():
@@ -19,8 +32,6 @@ def root():
         'status-code': HTTPStatus.OK,
     }
     return response
-
-
 
 
 @app.post('/predict/')
@@ -41,6 +52,5 @@ async def predict_label(file: UploadFile = File(...)):
     pred = model(image_tensor)
 
     label_num = pred.argmax(dim=-1)
-    label_encoder = joblib.load('data/processed/label_encoder.pkl')
     label = label_encoder.inverse_transform(label_num)[0]
     return {'dog_breed': label}
